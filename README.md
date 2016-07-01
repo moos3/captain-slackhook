febd78532b203399b1b7fb95c547ab0e80efc2ad
Richard Genther <richard.genthner@wheniwork.com>
2016-07-01 09:49:13 -0400

    updated the readme

--- README.md
+++ README.md
@@ -4,8 +4,11 @@ This is a simple bot for making webhook endpoints to avoid integations.
 ### Currently Supported
 #### endpoint /send
 Supports the following objects, to be posted using a json.
+You can set override the channel by specifying it before declaring events or a message.
 
 Event: (Slack Attachment Api)
+Supports sending multiple events at once. Supports any values that can been found in the attachment documentation for slack. Reference here [Api Doc](https://api.slack.com/docs/message-attachments)
+Priority and value will automatically mapped to the fields. If you can short but declaring it to override the default of False. 
 ```
 {
 	"event": [{
@@ -28,6 +31,4 @@ Message: (Simple message)
 }
 ```
 
-### Support Values in the Objects
-
-channel: Can be a channel or @username
+Calls will return a json object. That will contain ok set to True or False depending is the message send failed or was successful. Also will contian a message attribute with a message for logging.
--- main.py
+++ main.py
@@ -16,28 +16,31 @@ BOT_IMAGE_URL = os.getenv('BOT_IMAGE_URL', None)
 BOT_DEBUG = os.getenv('BOT_DEBUG',False)
 BOT_USERNAME = os.getenv('BOT_USERNAME', 'captainSlackHook')
 
-
 bot = Flask(__name__)
 slack_client = SlackClient(SLACK_TOKEN)
 
 def messageBuilder(data):
     if 'event' in data:
-        event_data = data['event'][0]
-        fields = { "short": False }
-        event_message = {"pretext":"Incomming Notification",
-                        "author_name":BOT_NAME,
-                        "fallback": "Event Triggered",
-        }
-        for k,v in event_data.iteritems():
-            if str(k) == 'priority' or str(k) == 'value':
-                if str(k) == 'priority':
-                    k = 'title'
-                fields[str(k)] = str(v)
-            else:
-                event_message[str(k)] = str(v)
+        message = []
+        for event_data in data['event']:
+        #    event_data = data['event'][0]
+            fields = { "short": False }
+            event_message = {"pretext":"Incomming Notification",
+                            "author_name":BOT_NAME,
+                            "fallback": "Event Triggered",
+            }
+            for k,v in event_data.iteritems():
+                if str(k) == 'priority' or str(k) == 'value':
+                    if str(k) == 'priority':
+                        k = 'title'
+                    fields[str(k)] = str(v)
+                elif str('k') == 'short':
+                    fields['short'] = v
+                else:
+                    event_message[str(k)] = str(v)
 
-        event_message['fields'] = [fields]
-        message = [ event_message ]
+            event_message['fields'] = [fields]
+            message.append(event_message)
     else:
         message = str(data['message'])
     return message
@@ -60,23 +63,22 @@ def datadog():
                 call = slack_client.api_call("chat.postMessage", channel=send_channel, username=BOT_USERNAME, icon_url=BOT_IMAGE_URL, attachments=json.dumps(message))
 
             if call['ok'] == True:
-                response_message = 'Sent Message to Slack'
+                response_message = json.dumps({"message":"Sent Message to Slack", "ok":True})
             else:
-                response_message = 'Failed to Send Message to Slack'
+                response_message = json.dumps({"message":"Failed to Send Message to Slack", "ok":False})
 
         else:
-            response_message = "Your json must have either a message object or attachemnt object. See documentation."
+            response_message = json.dumps({"message":"Your json must have either a message object or attachemnt object. See documentation.", "ok":False})
 
     else:
-        response_message = 'failed to authenticate'
+        response_message = json.dumps({"message":"Failed to Authenticate", "ok":False})
 
-    return Response('{"message":"' + response_message + '"}', mimetype="application/json"), 200
+    return Response(response_message, mimetype="application/json"), 200
 
 
 @bot.route('/', methods=['GET'])
 def test():
-  return Response('It works!')
-
+  return Response(json.dumps({"message":"It works!", "ok":True}), mimetype="application/json"), 200
 
 bot.wsgi_app = ProxyFix(bot.wsgi_app)
 application = bot
