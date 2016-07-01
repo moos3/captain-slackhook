01dc69f1a3358b60281b9508dbf4965a87ea6dd7
Richard Genther <richard.genthner@wheniwork.com>
2016-07-01 14:02:43 -0400

    added hipchat support

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from slackclient import SlackClient
 from werkzeug.contrib.fixers import ProxyFix
 from os.path import join, dirname
 from dotenv import load_dotenv
+import requests
+
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
@@ -15,6 +17,7 @@ BOT_NAME = os.getenv('BOT_NAME', None)
 BOT_IMAGE_URL = os.getenv('BOT_IMAGE_URL', None)
 BOT_DEBUG = os.getenv('BOT_DEBUG',False)
 BOT_USERNAME = os.getenv('BOT_USERNAME', 'captainSlackHook')
+HIPCHAT_API_TOKEN = os.getenv('HIPCHAT_API_TOKEN',None)
 
 bot = Flask(__name__)
 slack_client = SlackClient(SLACK_TOKEN)
@@ -47,8 +50,44 @@ def messageBuilder(data):
         message = str(data['message'])
     return message
 
+def hipchat_notify(room, message, color='yellow',
+                   notify=False, format='text', host='api.hipchat.com'):
+    if len(message) > 10000:
+        raise ValueError('Message too long')
+    if format not in ['text', 'html']:
+        raise ValueError("Invalid message format '{0}'".format(format))
+    if color not in ['yellow', 'green', 'red', 'purple', 'gray', 'random']:
+        raise ValueError("Invalid color {0}".format(color))
+    if not isinstance(notify, bool):
+        raise TypeError("Notify must be boolean")
+
+    url = "https://{0}/v2/room/{1}/notification".format(host, room)
+    headers = {'Content-type': 'application/json'}
+    headers['Authorization'] = "Bearer " + HIPCHAT_API_TOKEN
+    payload = {
+        'message': message,
+        'notify': notify,
+        'message_format': format,
+        'color': color
+    }
+    r = requests.post(url, data=json.dumps(payload), headers=headers)
+    r.raise_for_status()
+
+def hipchat_message(room, message, host='api.hipchat.com'):
+    if len(message) > 10000:
+        raise ValueError('Message too long')
+
+    url = "https://{0}/v2/room/{1}/message".format(host, room)
+    headers = {'Content-type': 'application/json'}
+    headers['Authorization'] = "Bearer " + HIPCHAT_API_TOKEN
+    payload = {
+        'message': message,
+    }
+    r = requests.post(url, data=json.dumps(payload), headers=headers)
+    r.raise_for_status()
+
 @bot.route('/send', methods=['POST'])
-def datadog():
+def send_slack():
     data = request.get_json()
     if str(data['token']) == BOT_WEBHOOK_SECRET:
         if 'channel' in data:
@@ -60,6 +99,9 @@ def datadog():
             message = messageBuilder(data)
             if 'message' in data:
                 call = slack_client.api_call("chat.postMessage", channel=send_channel, text=message, username=BOT_USERNAME, icon_url=BOT_IMAGE_URL)
+                if 'hipchat' in data:
+                    for r in data['hipchat']['rooms']:
+                        hipchat_message(r, send_message)
 
             if 'event' in data:
                 call = slack_client.api_call("chat.postMessage", channel=send_channel, username=BOT_USERNAME, icon_url=BOT_IMAGE_URL, attachments=json.dumps(message))
