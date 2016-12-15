5183dafb16f2fbabd64ba281b8c75c6c827a3fca
Richard Genther <richard.genthner@wheniwork.com>
2016-12-15 15:54:12 -0500

    new class based

--- main.py
+++ main.py
@@ -30,16 +30,66 @@ BOT_USERNAME = os.getenv('BOT_USERNAME', 'captainSlackHook')
 HIPCHAT_API_TOKEN = os.getenv('HIPCHAT_API_TOKEN',None).replace("\'","") # replace is because how new python version wrap in single quotes
 
 bot = Flask(__name__)
-slack_client = SlackClient(SLACK_TOKEN)
 http = urllib3.PoolManager()
 
-def messageBuilder(data):
-    if 'event' in data:
+
+class Base(object):
+
+    def __init__(self,token,bot_name, bot_image_url,bot_username):
+        self.token = token
+        self.bot_name = bot_name
+        self.bot_image_url = bot_image_url
+        self.bot_username = bot_username
+
+    @property
+    def bot_username(self):
+        return self.bot_username
+
+    @property
+    def token(sefl):
+        return self.token
+
+    @property
+    def bot_name(self):
+        return self.name
+
+    @property
+    def bot_image_url(self):
+        return self.bot_image_url
+
+    @bot_image_url.setter
+    def bot_image_url(self, image_url):
+        self.bot_image_url = image_url
+
+    @bot_name.setter
+    def bot_name(self, name):
+        self.bot_name = name
+
+    @token.setter
+    def token(self, token):
+        self.token = token
+
+    def error_msg(value):
+        return {
+            401: {"error": {"code":401, "message":"Authenticated requests only.", "type":"Unauthorized"} },
+            200: {"success": {"code":200, "message":"Sent Message successfully", "type":"Ok"}}
+        }[value]
+
+class Slack(Base):
+    method = "chat.postMessage"
+
+    def __init__(self,token, bot_name, bot_image_url, bot_username):
+        Base.__init__(self, token, bot_name, bot_image_url, bot_username)
+
+    def connect(self):
+        self.slack_client = SlackClient(self.token)
+
+    def build_event(data):
         message = []
         for event_data in data['event']:
             fields = []
             event_message = {"pretext":"Incomming Notification",
-                            "author_name":BOT_NAME,
+                            "author_name":self.bot_name,
                             "fallback": "Event Triggered",
             }
             for k,v in event_data.iteritems():
@@ -55,143 +105,142 @@ def messageBuilder(data):
                 else:
                     event_message[str(k)] = str(v)
 
-            event_message['fields'] = fields
-            message.append(event_message)
+                event_message['fields'] = fields
+                message.append(event_message)
+        return json.dumps(message)
+
+    @channel.setter
+    def channel(self,channel):
+        self.channel = channel
+
+    @property
+    def channel(self):
+        return self.channel
+
+    @property
+    def method(self):
+        return self.method
+
+    def send_message(self,message,room):
+        value = self.slack_client.api_call(self.slack_function,channel=room, username=self.bot_username(), icon_url=self.bot_image_url(), message=message)
+        return value
+
+    def send_event(self,payload,room):
+        event = build_event(payload)
+        value = self.slack_client.api_call(self.method,channel=room, username=self.bot_username(), icon_url=self.bot_image_url(), attachments=payload)
+        return value
+
+
+class Hipchat(Base):
+
+    def __init__(self,token, bot_name, bot_image_url, bot_username):
+        Base.__init__(self, token, bot_name, bot_image_url, bot_username)
+
+    def send(payload, url):
+        http = urllib3.PoolManager()
+        headers = {'Content-type': 'application/json'}
+        headers['Authorization'] = "Bearer " + HIPCHAT_API_TOKEN
+        r = http.request('POST', url, body=json.dumps(payload).encode('UTF-8'), headers=headers,timeout=20)
+        go = self.error_handler(r)
+        return go
+
+    def build_notify(room, message, color='yellow',
+                       notify=False, format='text', host='api.hipchat.com'):
+        if len(message) > 10000:
+            raise ValueError('Message too long')
+        if format not in ['text', 'html']:
+            raise ValueError("Invalid message format '{0}'".format(format))
+        if color not in ['yellow', 'green', 'red', 'purple', 'gray', 'random']:
+            raise ValueError("Invalid color {0}".format(color))
+        if not isinstance(notify, bool):
+            raise TypeError("Notify must be boolean")
+
+        url = "https://{0}/v2/room/{1}/notification".format(host, room)
+        payload = {
+            'message': message,
+            'notify': notify,
+            'message_format': format,
+            'color': color
+        }
+        return (payload, url)
+
+    def build_message(room, message, host='api.hipchat.com'):
+        if len(message) > 10000:
+            raise ValueError('Message too long')
+
+        url = "https://{0}/v2/room/{1}/message".format(host, room)
+        payload = {
+            'message': message,
+        }
+        return (payload, url)
+
+    def error_handler(self,r):
+        status = r.status
+        if status == 401:
+            return self.error_msg(status)
+        else if status in [200,204,201]:
+            return {"success":{"code":status, "message": "Message send secuessful", "type":"seccussful"}}
+        else:
+            return {"error":{"code":"405", "message":"Unable to handle request", "type":"bad_method"}}
+
+
+def authorization(token):
+    if data['token'] == BOT_WEBHOOK_SECRET:
+        return True
     else:
-        message = str(data['message'])
-    return message
-
-def send_hipchat_payload(payload, url):
-    headers = {'Content-type': 'application/json'}
-    headers['Authorization'] = "Bearer " + HIPCHAT_API_TOKEN
-    r = http.request('POST', url, body=json.dumps(payload).encode('UTF-8'), headers=headers,timeout=20)
-    return (r.status, r.data)
-
-def hipchat_notify(room, message, color='yellow',
-                   notify=False, format='text', host='api.hipchat.com'):
-    if len(message) > 10000:
-        raise ValueError('Message too long')
-    if format not in ['text', 'html']:
-        raise ValueError("Invalid message format '{0}'".format(format))
-    if color not in ['yellow', 'green', 'red', 'purple', 'gray', 'random']:
-        raise ValueError("Invalid color {0}".format(color))
-    if not isinstance(notify, bool):
-        raise TypeError("Notify must be boolean")
-
-    url = "https://{0}/v2/room/{1}/notification".format(host, room)
-    payload = {
-        'message': message,
-        'notify': notify,
-        'message_format': format,
-        'color': color
-    }
-    status, data = send_hipchat_payload(payload, url)
-    return (status, data)
-
-def hipchat_message(room, message, host='api.hipchat.com'):
-    if len(message) > 10000:
-        raise ValueError('Message too long')
-
-    url = "https://{0}/v2/room/{1}/message".format(host, room)
-    payload = {
-        'message': message,
-    }
-    status, data = send_hipchat_payload(payload, url)
-    return (status, data)
-
-def slack_message(data):
-    if 'channel' in data:
-        send_channel = data['channel']
-    else:
-        send_channel = '#general'
-
-    if 'message' in data or 'event' in data:
-        message = messageBuilder(data)
-        if 'message' in data:
-            call = slack_client.api_call("chat.postMessage", channel=send_channel, text=message, username=BOT_USERNAME, icon_url=BOT_IMAGE_URL)
-            if 'hipchat' in data:
-                if 'notify' in data['hipchat']:
-                    for r in data['hipchat']['rooms']:
-                        hipchat = hipchat_notify(r, message, data['hipchat']['notify']['color'], True)
-                else:
-                    for r in data['hipchat']['rooms']:
-                        hipchat = hipchat_message(r, message)
-
-def slack_end_event(data):
-    if 'channel' in data:
-        send_channel = data['channel']
-    else:
-        send_channel = '#general'
-
-    if 'event' in data:
-        message = messageBuilder(data)
-        call = slack_client.api_call("chat.postMessage", channel=send_channel, username=BOT_USERNAME, icon_url=BOT_IMAGE_URL, attachments=json.dumps(message))
-
-def hipchat_process_message(data):
-    if 'notify' in data['hipchat']:
-        for r in data['hipchat']['rooms']:
-            status, data = hipchat_notify(r, message, data['hipchat']['notify']['color'], True)
-            if status == '401':
-                break
-        return data
-    else:
-        for r in data['hipchat']['rooms']:
-            status, data = hipchat_message(r, message)
-            if status == '401':
-                break
-        return data
+        return False
 
 @bot.route('/send', methods=['POST'])
-def send_slack():
-    call = []
+def send_messages():
+
     data = request.get_json()
-    print BOT_WEBHOOK_SECRET
-    print data
-    if data['token'] == BOT_WEBHOOK_SECRET:
-        print "tokens match"
+
+    if authorization(data['token']):
+        if data['slack']:
+            slack_data = data['slack']
+            slack_client = Slack(SLACK_TOKEN, BOT_NAME, BOT_IMAGE_URL,BOT_USERNAME)
+
+            if slack_data['message']['type'] == 'event':
+                if len(slack_data['message']['rooms']) == 1
+                    slack_client.send_event(slack_data['message'])
+                elif len(slack_data['message']['rooms']) > 1:
+                    for r in slack_data['message']['rooms']:
+                        slack_client.send_event(slack_data['message'])
+
+            if slack_data['message']['type'] == 'message':
+                if len(slack_data['message']['rooms']) == 1:
+                    slack_client.send_message(slack_data['message'])
+                elif len(slack_data['message']['rooms']) > 1:
+                    for r in slack_data['message']['rooms']:
+                        slack_client.send_message(slack_data['message'])
+
+        if data['hipchat']:
+            hipchat_data = data['hipchat']
+            hipchat_client = Hipchat(HIPCHAT_API_TOKEN, BOT_NAME, BOT_IMAGE_URL, BOT_USERNAME)
+            if hipchat_data['message']['type'] == 'notify':
+                if len(hipchat_data['message']['rooms']) == 1
+                    payload, url = hipchat_client.build_notify(hipchat_data['message'])
+                    d = hipchat_client.send(payload, url)
+                    if d['error']:
+                        return Response(json.dumps(d), mimetype="application/json"), d['error']['code']
+
+                else if len(hipchat_data['message']['rooms']) > 1:
+                    for r in hipchat_data['message']['rooms']:
+                        payload, url = hipchat_client.build_notify(hipchat_data['message'])
+                        d = hipchat_client.send(payload, url)
+                        if d['error']:
+                            break
+                    return Response(json.dumps(d), mimetype="application/json"), d['error']['code']
+
+
     else:
-        print "Token given " + data['token']
-        print "Secret webhook " + str(BOT_WEBHOOK_SECRET)
-
-    if str(data['token']) == BOT_WEBHOOK_SECRET:
-        if 'channel' in data:
-            send_channel = data['channel']
-        else:
-            send_channel = '#general'
-
-        if 'message' in data or 'event' in data:
-            message = messageBuilder(data)
-            if 'message' in data:
-                call = slack_client.api_call("chat.postMessage", channel=send_channel, text=message, username=BOT_USERNAME, icon_url=BOT_IMAGE_URL)
-                print call
-                if 'hipchat' in data:
-                    if 'notify' in data['hipchat']:
-                        for r in data['hipchat']['rooms']:
-                            hipchat = hipchat_notify(r, message, data['hipchat']['notify']['color'], True)
-                    else:
-                        for r in data['hipchat']['rooms']:
-                            hipchat = hipchat_message(r, message)
-
-            if 'event' in data:
-                call = slack_client.api_call("chat.postMessage", channel=send_channel, username=BOT_USERNAME, icon_url=BOT_IMAGE_URL, attachments=json.dumps(message))
-
-            if call['ok'] == True:
-                response_message = json.dumps({"message":"Sent Message to Slack and Hipchat", "ok":True})
-            else:
-                response_message = json.dumps({"message":"Failed to Send Message to Slack or Hipchat", "ok":False})
-
-        else:
-            response_message = json.dumps({"message":"Your json must have either a message object or attachemnt object. See documentation.", "ok":False})
-
-    else:
-        response_message = json.dumps({"message":"Failed to Authenticate", "ok":False})
-
-    return Response(response_message, mimetype="application/json"), 200
+        message = {"error": {"code":401, "message":"Authenticated requests only.", "type":"Unauthorized"} }
+        return Response(json.dumps(error), mimetype="application/json"), message['error']['code']
 
 
 @bot.route('/', methods=['GET'])
-def test():
-  return Response(json.dumps({"message":"It works!", "ok":True}), mimetype="application/json"), 200
+def index():
+  return Response(json.dumps({"success":{"message":"It works! Please see the docs to use!", "code":200, "type":"successful"}), mimetype="application/json"), 200
 
 bot.wsgi_app = ProxyFix(bot.wsgi_app)
 application = bot
